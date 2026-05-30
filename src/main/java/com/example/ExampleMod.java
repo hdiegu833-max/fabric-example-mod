d5e68b4538e68140b17ead74f6245d6d1cec2a10
hdiegu833-max <hdiegu833@gmail.com>
2026-05-30 03:54:54 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,28 @@
-package com.example;
+package com.beshoy.tracer;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class TracerMod implements ClientModInitializer {
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    public static boolean enabled = true;
+    public static KeyBinding toggleKey;
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitializeClient() {
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        toggleKey = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.tracer.toggle",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_X,
+                        "category.tracer"
+                )
+        );
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        TracerRenderer.register();
+    }
 }
